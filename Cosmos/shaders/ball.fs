1b8e457ec7acad7e3dfdc2e77ee106abb2f72f0c
ljfcnyali <ljfcnyali@gmail.com>
2022-12-10 14:02:00 +0800

    Phong Lighting Model

--- Cosmos/shaders/ball.fs
+++ Cosmos/shaders/ball.fs
@@ -19,8 +19,6 @@ uniform float ao;
 uniform vec3 lightPos;
 uniform vec3 viewPos;
 
-const float PI = 3.14159265359;
-
 vec3 getNormalFromMap()
 {
     vec3 tangentNormal = texture(normal, fs_in.TexCoords).xyz * 2.0 - 1.0;
@@ -38,84 +36,23 @@ vec3 getNormalFromMap()
     return normalize(TBN * tangentNormal);
 }
 
-vec3 fresnelSchlick(float cosTheta, vec3 F0) { return F0 + (1.0 - F0) * pow(1.0 - cosTheta, 5.0); }  
-
-float DistributionGGX(vec3 N, vec3 H, float roughness)
+vec3 CalcPointLight(vec3 light, vec3 normal, vec3 fragPos, vec3 viewDir)
 {
-    float a = roughness*roughness, a2 = a * a;
-    float NdotH  = max(dot(N, H), 0.0), NdotH2 = NdotH * NdotH;
-    float nom = a2, denom = (NdotH2 * (a2 - 1.0) + 1.0);
-    denom = PI * denom * denom;
-    return nom / denom;
+    vec3 lightDir = normalize(lightPos - fragPos);
+    vec3 halfwayDir = normalize(lightDir + viewDir);
+    float diff = max(dot(normal, lightDir), 0.0);
+    float spec = pow(max(dot(normal, halfwayDir), 0.0), 64);
+    vec3 ambient = 0.05 * pow(Albedo, vec3(2.2));
+    vec3 diffuse = 2.0 * diff * pow(Albedo, vec3(2.2));
+    vec3 specular = 1.0 * spec * vec3(1.0f);
+    return ambient + diffuse + specular;
 }
-
-float GeometrySchlickGGX(float NdotV, float roughness)
-{
-    float r = (roughness + 1.0), k = (r * r) / 8.0;
-    float nom = NdotV, denom = NdotV * (1.0 - k) + k;
-    return nom / denom;
-}
-
-float GeometrySmith(vec3 N, vec3 V, vec3 L, float roughness)
-{
-    float NdotV = max(dot(N, V), 0.0), NdotL = max(dot(N, L), 0.0);
-    float ggx2  = GeometrySchlickGGX(NdotV, roughness), ggx1  = GeometrySchlickGGX(NdotL, roughness);
-    return ggx1 * ggx2;
-}
-
-vec3 fresnelSchlickRoughness(float cosTheta, vec3 F0, float roughness)
-{
-    return F0 + (max(vec3(1.0 - roughness), F0) - F0) * pow(clamp(1.0 - cosTheta, 0.0, 1.0), 5.0);
-}   
 
 void main()
 {
-    vec3 albedo = pow(Albedo, vec3(2.2));  
-
-    vec3 N = getNormalFromMap(), V = normalize(viewPos - fs_in.FragPos), R = reflect(-V, N);
-    vec3 Lo = vec3(0.0), F0 = vec3(0.04); F0 = mix(F0, albedo, metallic);
-
-    for ( int i = 0; i < 1; ++ i )
-    {
-        vec3 lightColor = vec3(3.0);
-        vec3 L = normalize(lightPos - fs_in.FragPos), H = normalize(V + L);
-        float distance = length(lightPos - fs_in.FragPos), attenuation = 1.0 / (distance * distance);
-        vec3 radiance = lightColor * attenuation;
-
-		vec3 F = fresnelSchlick(max(dot(H, V), 0.0), F0);
- 
-        float NDF = DistributionGGX(N, H, roughness), G = GeometrySmith(N, V, L, roughness);      
-        vec3 nominator = NDF * G * F;
-		float denominator = 4.0 * max(dot(N, V), 0.0) * max(dot(N, L), 0.0) + 0.001; 
-		vec3 specular = nominator / denominator;  
- 
-		vec3 kS = F, kD = vec3(1.0) - kS; kD *= 1.0 - metallic;   
-        float NdotL = max(dot(N, L), 0.0);        
-        Lo += (kD * albedo / PI + specular) * radiance * NdotL;
-    }
-
-    vec3 F = fresnelSchlickRoughness(max(dot(N, V), 0.0), F0, roughness);
-
-    vec3 kS = fresnelSchlick(max(dot(N, V), 0.0), F0);
-    vec3 kD = 1.0 - kS;
-    kD *= 1.0 - metallic;	  
-    vec3 irradiance = texture(cubemap, N).rgb;
-    vec3 diffuse      = irradiance * albedo;
-
-    const float MAX_REFLECTION_LOD = 4.0;
-    vec3 prefilteredColor = textureLod(cubemap2, R,  roughness * MAX_REFLECTION_LOD).rgb;    
-    vec2 brdf  = texture(brdf, vec2(max(dot(N, V), 0.0), roughness)).rg;
-    vec3 specular = prefilteredColor * (F * brdf.x + brdf.y);
-
-    vec3 ambient = (kD * diffuse + specular) * ao;
-
-    ambient = specular * ao;
-    ambient = vec3(0.03) * albedo * ao;
-
-	vec3 color = ambient + Lo;  
-
+    vec3 norm = getNormalFromMap(), viewDir = normalize(viewPos - fs_in.FragPos);
+    vec3 color = CalcPointLight(lightPos, norm, fs_in.FragPos, viewDir);
     color = color / (color + vec3(1.0));
     color = pow(color, vec3(1.0/2.2));  
-
     FragColor = vec4(color, 1.0);
 }
